326e4212e10358229682d94dfbdf34eb7115aac7
Kamesuta <kamesuta@gmail.com>
2023-05-20 03:30:57 +0900

    デバッグ用に点を描画 + 惜しいところまで調整

--- src/main/java/quarri6343/graduationphoto/ProjectionUtility.java
+++ src/main/java/quarri6343/graduationphoto/ProjectionUtility.java
@@ -4,36 +4,25 @@ import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.math.vector.Vector4f;
-import org.lwjgl.opengl.GL11;
 
 public final class ProjectionUtility {
-    
-    private static Matrix4f viewProjectionMatrix;
-    
-    public static Vector3f worldToViewportPosition(Vector3d posIn) {
-        final Vector4f vector4f = new Vector4f((float)posIn.x, (float)posIn.y, (float)posIn.z, 1f);
 
-        vector4f.transform(viewProjectionMatrix);
+    private static Matrix4f viewModelMatrix;
+    private static Matrix4f projectionMatrix;
+
+    public static Vector3f worldToViewportPosition(Matrix4f viewModelMatrix, Vector3d posIn) {
+        final Vector4f vector4f = new Vector4f((float) posIn.x / 3f, (float) posIn.y / 3f, (float) posIn.z / 3f, 1f);
+
+        vector4f.transform(viewModelMatrix);
+        vector4f.transform(projectionMatrix);
 
         final float invert = 1f / vector4f.w();
-        float x = vector4f.x();
-        float y = vector4f.y();
-        vector4f.setX(x * invert);
-        vector4f.setY(y * invert);
 
-        return new Vector3f(vector4f.x(), vector4f.y(), vector4f.z());
+        return new Vector3f(vector4f.x() * invert, vector4f.y() * invert, vector4f.z() * invert);
     }
 
-    public static void updateViewProjectionMatrix() {
-        float[] modelview = new float[16];
-        float[] projection = new float[16];
-        GL11.glGetFloatv(GL11.GL_MODELVIEW_MATRIX, modelview);
-        GL11.glGetFloatv(GL11.GL_PROJECTION_MATRIX, projection);
-
-        Matrix4f modelMatrix = new Matrix4f(modelview);
-        Matrix4f projectionMatrix = new Matrix4f(projection);
-
-        modelMatrix.multiply(projectionMatrix);
-        viewProjectionMatrix =  modelMatrix;
+    public static void updateViewProjectionMatrix(Matrix4f viewModelMatrix, Matrix4f projectionMatrix) {
+        ProjectionUtility.viewModelMatrix = viewModelMatrix;
+        ProjectionUtility.projectionMatrix = projectionMatrix;
     }
 }
--- src/main/java/quarri6343/graduationphoto/RenderPlayerEventListener.java
+++ src/main/java/quarri6343/graduationphoto/RenderPlayerEventListener.java
@@ -1,9 +1,14 @@
 package quarri6343.graduationphoto;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -11,8 +16,12 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.lwjgl.opengl.GL11.GL_QUADS;
+
 public class RenderPlayerEventListener {
     public static List<PlayerEntity> playersOnPhotoFrame = new ArrayList<>();
+
+    private Vector3f viewportPosition;
 
     @SubscribeEvent
     public void onRenderPlayer(RenderPlayerEvent.Post event) {
@@ -20,8 +29,10 @@ public class RenderPlayerEventListener {
             return;
         }
 
-        Vector3f viewportPosition = ProjectionUtility.worldToViewportPosition(event.getPlayer().position());
-        
+        Matrix4f viewModelMatrix = event.getMatrixStack().last().pose();
+        Vector3f viewportPosition = ProjectionUtility.worldToViewportPosition(viewModelMatrix, event.getPlayer().position());
+        this.viewportPosition = viewportPosition;
+
         if (viewportPosition.x() >= -0.4 && viewportPosition.x() <= 0.4
                 && viewportPosition.y() >= -0.6 && viewportPosition.y() <= 0.6) {
             playersOnPhotoFrame.add(event.getPlayer());
@@ -30,6 +41,40 @@ public class RenderPlayerEventListener {
 
     @SubscribeEvent
     public void onRenderWorld(RenderWorldLastEvent event) {
-        ProjectionUtility.updateViewProjectionMatrix();
+        Matrix4f viewModelMatrix = event.getMatrixStack().last().pose();
+        Matrix4f projectionMatrix = event.getProjectionMatrix();
+        ProjectionUtility.updateViewProjectionMatrix(viewModelMatrix, projectionMatrix);
+    }
+
+    @SubscribeEvent
+    public void onRenderGui(RenderGameOverlayEvent event) {
+        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL) {
+            if (viewportPosition != null) {
+                // スクリーンのサイズ取得
+                int screenWidth = Minecraft.getInstance().getWindow().getGuiScaledWidth();
+                int screenHeight = Minecraft.getInstance().getWindow().getGuiScaledHeight();
+
+                // ビューポートの位置をスクリーン座標に変換
+                float x = (viewportPosition.x() + 1) * screenWidth / 2f;
+                float y = (-viewportPosition.y() + 1) * screenHeight / 2f;
+
+                // 点を描画
+                RenderSystem.disableTexture();
+
+                Tessellator t = Tessellator.getInstance();
+                BufferBuilder builder = t.getBuilder();
+                builder.begin(GL_QUADS, DefaultVertexFormats.POSITION);
+                //builder.vertex(viewportPosition.x(), viewportPosition.y(), 0).color(255, 0, 0, 255).endVertex();
+
+                builder.vertex(x - 4f, y + 4f, -90.0D).endVertex();
+                builder.vertex(x + 4f, y + 4f, -90.0D).endVertex();
+                builder.vertex(x + 4f, y - 4f, -90.0D).endVertex();
+                builder.vertex(x - 4f, y - 4f, -90.0D).endVertex();
+
+                t.end();
+
+                RenderSystem.enableTexture();
+            }
+        }
     }
 }
